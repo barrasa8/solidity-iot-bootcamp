cc8097ca3664556dd548f57733683651b95006ba
barrasa8 <barrasa8@hotmail.com>
2020-09-06 14:33:05 +1000

    Bonus assignment -> mapping of number of people created by address

--- week-1/Day-1/PeopleArray.sol
+++ week-1/Day-1/PeopleArray.sol
@@ -12,6 +12,7 @@ contract HelloWorld{
 
     //mapping (address => Person) private people;
     Person[] private people;
+    mapping (address=>uint) private peopleBalance;
 
     function createPerson(string memory name, uint age, uint height) public {
         //This creates a person
@@ -34,10 +35,10 @@ contract HelloWorld{
         //address creator = msg.sender;
         //people[creator] = newPerson;
         people.push(newPerson);
+        peopleBalance[msg.sender]=peopleBalance[msg.sender]+1;
     }
-    function getPerson(uint index) public view returns(address addr, string memory name, uint age, uint height, bool senior){
+    function getPerson(uint index) public view returns(address addr, string memory name, uint age, uint height, bool senior,uint numberOfPeople){
         //address creator = msg.sender;
-        return (people[index].addr,people[index].name, people[index].age, people[index].height, people[index].senior);
+        return (people[index].addr,people[index].name, people[index].age, people[index].height, people[index].senior, peopleBalance[people[index].addr]);
     }
-
 }
